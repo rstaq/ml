91e7821fa090812514e1505b8a9a27fb5b19efcc
rwenig <rwenig@student.tugraz.at>
2018-09-02 20:40:16 +0200

    implement training loop

--- nn/digit_classification.py
+++ nn/digit_classification.py
@@ -12,6 +12,11 @@ def plot_rand_digits(X, y):
         plt.show()
 
 
+def plot_cost_function(J_costs):
+    plt.plot(J_costs, 'b-')
+    plt.show()
+
+
 def rand_init_theta(rows, cols, epsilon=0.12):
     return np.random.rand(rows, cols) * (2 * epsilon) - epsilon
 
@@ -67,12 +72,12 @@ def cost_function(X, y, theta_1, theta_2, lambda_=0):
 
     # regularization
     J += (lambda_ / (2 * m)) * ( np.sum(np.power(theta_2[:,1:], 2)) + np.sum(np.power(theta_1[:,1:], 2)))
-    theta_1_reg = theta_1
-    theta_2_reg = theta_2
+    theta_1_reg = np.array(theta_1, copy=True)
+    theta_2_reg = np.array(theta_2, copy=True)
     theta_1_reg[:,0] = 0
     theta_2_reg[:,0] = 0
-    theta_1_new += (lambda_ / m * theta_1_reg)
-    theta_2_new += (lambda_ / m * theta_2_reg)
+    theta_1_new = np.add(theta_1_new, lambda_ / m * theta_1_reg)
+    theta_2_new = np.add(theta_2_new, lambda_ / m * theta_2_reg)
 
     return J, theta_1_new, theta_2_new
 
@@ -81,6 +86,7 @@ def main():
     input_layer_size = 400
     hidden_layer_size = 25
     output_layer_size = 10
+    max_iter = 50
 
     data = sio.loadmat("data.mat")
     X = data.get('X')
@@ -90,7 +96,9 @@ def main():
     theta_1 = rand_init_theta(hidden_layer_size, input_layer_size + 1)
     theta_2 = rand_init_theta(output_layer_size, hidden_layer_size + 1)
 
-    J, grad = cost_function(X, y, theta_1, theta_2)
+    for i in range(max_iter):
+        J, theta_1, theta_2 = cost_function(X, y, theta_1, theta_2, 1)
+        print("Iteration {} cost {}".format(i, J))
 
 
 if __name__ == '__main__':
