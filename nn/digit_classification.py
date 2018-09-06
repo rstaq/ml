9ffd9c30e6629db4cd319fbaf507b4fd402d84ba
rwenig <rwenig@student.tugraz.at>
2018-09-06 16:31:35 +0200

    implement the training loop
    The nn gets trained for the number of max_iter. After training the
    costs get plotted to ensure that the costs converge.

--- nn/digit_classification.py
+++ nn/digit_classification.py
@@ -13,7 +13,10 @@ def plot_rand_digits(X, y):
 
 
 def plot_cost_function(J_costs):
-    plt.plot(J_costs, 'b-')
+    plt.plot(J_costs, 'k-', linewidth=1)
+    plt.title("J(θ) after every iteration")
+    plt.xlabel("number iteration")
+    plt.ylabel("J(θ)")
     plt.show()
 
 
@@ -44,8 +47,8 @@ def convert_y(y):
 def cost_function(X, y, theta_1, theta_2, lambda_=0):
     m = len(X)
     J = 0
-    theta_1_new = np.zeros((len(theta_1), len(theta_1.T)))
-    theta_2_new = np.zeros((len(theta_2), len(theta_2.T)))
+    theta_1_gradient = np.zeros((len(theta_1), len(theta_1.T)))
+    theta_2_gradient = np.zeros((len(theta_2), len(theta_2.T)))
 
     X = np.c_[np.ones(len(X)), X]
     for i in range(m):
@@ -64,11 +67,11 @@ def cost_function(X, y, theta_1, theta_2, lambda_=0):
         delta2 = np.delete(delta2, 0)
         delta2 = delta2 * sigmoid_gradient(z2)
 
-        theta_1_new += np.outer(delta2, a1)
-        theta_2_new += np.outer(delta3, a2)
+        theta_1_gradient += np.outer(delta2, a1)
+        theta_2_gradient += np.outer(delta3, a2)
 
-    theta_1_new /= m
-    theta_2_new /= m
+    theta_1_gradient /= m
+    theta_2_gradient /= m
 
     # regularization
     J += (lambda_ / (2 * m)) * ( np.sum(np.power(theta_2[:,1:], 2)) + np.sum(np.power(theta_1[:,1:], 2)))
@@ -76,29 +79,38 @@ def cost_function(X, y, theta_1, theta_2, lambda_=0):
     theta_2_reg = np.array(theta_2, copy=True)
     theta_1_reg[:,0] = 0
     theta_2_reg[:,0] = 0
-    theta_1_new = np.add(theta_1_new, lambda_ / m * theta_1_reg)
-    theta_2_new = np.add(theta_2_new, lambda_ / m * theta_2_reg)
+    theta_1_gradient = np.add(theta_1_gradient, lambda_ / m * theta_1_reg)
+    theta_2_gradient = np.add(theta_2_gradient, lambda_ / m * theta_2_reg)
 
-    return J, theta_1_new, theta_2_new
+    return J, theta_1_gradient, theta_2_gradient
 
 
 def main():
     input_layer_size = 400
     hidden_layer_size = 25
     output_layer_size = 10
-    max_iter = 50
+    max_iter = 400
+    lambda_ = 1
 
     data = sio.loadmat("data.mat")
     X = data.get('X')
     y = data.get('y')
     # plot_rand_digits(X, y)
 
+    print("Randomly initializing theta")
     theta_1 = rand_init_theta(hidden_layer_size, input_layer_size + 1)
     theta_2 = rand_init_theta(output_layer_size, hidden_layer_size + 1)
+    J_costs = []
 
+    # train theta
     for i in range(max_iter):
-        J, theta_1, theta_2 = cost_function(X, y, theta_1, theta_2, 1)
+        J, theta_1_gradient, theta_2_gradient = cost_function(X, y, theta_1, theta_2, lambda_)
+        theta_1 -= theta_1_gradient
+        theta_2 -= theta_2_gradient
+        J_costs.append(J)
         print("Iteration {} cost {}".format(i, J))
+
+    plot_cost_function(J_costs)
 
 
 if __name__ == '__main__':
